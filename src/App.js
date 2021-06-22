abc2789993f73e2e7ca51a0d7e44e2be134b9018
Indra <indra@ide.web.id>
2021-06-22 16:01:57 +0700

    add redux equipment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {store} from './store';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './navigations/BottomTabNavigator';
 import { DashboardStackNavigator } from './navigations/StackNavigator';
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<DashboardStackNavigator />
-		</NavigationContainer>
+		<Provider store={store}>
+			<NavigationContainer>
+				<DashboardStackNavigator />
+			</NavigationContainer>
+		</Provider>
 	);
 };
